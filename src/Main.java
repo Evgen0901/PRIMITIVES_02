b2e77dcf7b76d07cf3bdfd866caad44ddc9cf261
Eugene <konavalov.evgen@yandex.ru>
2025-07-10 23:18:42 +0300

    add Main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int replenishment = 1100;
         int bonus = replenishment / 100;
 
-        if (replenishment >= 1000) {
+        if (replenishment > 1000) {
             System.out.println(account + replenishment + bonus);
         } else {
             System.out.println(account + replenishment);
