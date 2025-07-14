584f6ebeeaf429b2d811df813005f005a62a3be6
Eugene <konavalov.evgen@yandex.ru>
2025-07-14 11:34:04 +0300

    bonus amount

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ public class Main {
         int account = 100;
         int replenishment = 1100;
         int bonus = replenishment / 100;
+        int total = account + replenishment + bonus;
 
         if (replenishment > 1000) {
-            System.out.println(account + replenishment + bonus);
+            System.out.println("total: " + total);
+            System.out.println("bonus amount: " + bonus);
         } else {
             System.out.println(account + replenishment);
         }
